57e825dd7a1eea147653942fcd082fa0e03975d1
Irakli Gubelidze <igubelidze@ip.local>
2022-07-19 13:30:08 +0400

    Convert Kilometers to Miles

---
+++ km-mile.js
@@ -0,0 +1,20 @@
+// miles = kilometers *
+
+const kilometers = prompt('Enter value in kilometers: ');
+
+// conversion factor
+const factor = 0.621371;
+
+// calculate miles 
+const miles = kilometers * factor;
+
+console.log(`${kilometers} kilometers is equal to ${miles} miles.`)
+
+// Output
+// Enter value in kilometers: 2.2
+// 2.2 kilometers is equal to 1.3670162000000001 miles.
+
+
+// To convert miles to kilometers, you can use formula:
+
+// kilometers = miles / factor
