3fb873222bd71660cf6a64a555ef19ba594db3e2
Irakli Gubelidze <igubelidze@ip.local>
2022-07-20 16:15:25 +0400

    Convert Celsius to Fahrenheit

---
+++ celsius-fahrenheit.js
@@ -0,0 +1,10 @@
+// fahrenheit = celsius * 1.8 + 32
+
+// Example: Celsius to Fahrenheit
+
+const celsius = prompt('Enter a celsius value: ');
+
+// calculate fahrenheit
+const fahrenheit = (celsius * 1.8) + 32;
+
+console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit`)
